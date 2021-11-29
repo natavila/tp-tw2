bfe857d5009a25f5ea91a4058fce5b232c229e53
AngeloSalvador7 <aordonezgarcia@alumno.unlam.edu.ar>
2021-11-28 21:38:17 -0300

    Se actualizo el helper validador de JWT

--- backend/src/helpers/validar-jwt.js
+++ backend/src/helpers/validar-jwt.js
@@ -2,7 +2,6 @@ const JWT = require('jsonwebtoken');
 const { Usuario } = require('../models/usuario');
 
 const validarJwt = (req, res, next) => {
-
     const mensaje = 'Solicitud denegada - Unauthorized';
 
     if(!req.headers.authorization || !req.headers.authorization.split(' ')[1])
@@ -19,17 +18,16 @@ const validarJwt = (req, res, next) => {
     }
 
     Usuario.findById(payload._id)
-        .then(usuario => {
-            if(!usuario)
-                return res.status(401).send({ mensaje });
-        
-            req.userId = payload._id;
-
-            next();
-        })
-        .catch(error => {
-            return res.status(400).send({ mensaje: 'Error', error })
-        })
+    .then(usuario => {
+        if(!usuario)
+            return res.status(401).send({ mensaje });
+    
+        req.userId = payload._id;
+        next();
+    })
+    .catch(error => {
+        return res.status(400).send({ mensaje: 'Error', error })
+    })
 }
 
 module.exports = { validarJwt }
