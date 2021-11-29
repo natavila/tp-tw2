b91145975d9a7db0c11546a3c2f17a3301d9c3af
AngeloSalvador7 <aordonezgarcia@alumno.unlam.edu.ar>
2021-11-29 00:55:10 -0300

    Se actualizo el archivo de configuracion de la db para que trabaje con .then y .catch

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -3,21 +3,18 @@ const mongoose = require('mongoose');
 const puertoDB = '27017';
 const nombreDB = 'videojuegosdb';
 
-const dbConexion = async () => {
-
-    try {
-
-        await mongoose.connect(`mongodb://localhost:${puertoDB}/${nombreDB}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-
+const dbConexion = () => {
+    mongoose.connect(`mongodb://localhost:${puertoDB}/${nombreDB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
         console.log('Conexion exitosa con la base de datos.')
-
-    } catch (error) {
+    })
+    .catch(error => {
         console.log('Error en la conexion con la base de datos, ERROR: ', error);
         throw new Error('Error en la conexion con la base de datos, ERROR: ', error);
-    }
+    })
 }
 
 module.exports = { dbConexion }
